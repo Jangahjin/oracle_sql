697baaef28537ac6d88c5b0e5836c174141c1536
Jangahjin <snita8379gmail.com>
2026-07-13 12:08:55 +0900

    2026 07 13

--- 20springboard_project.sql
+++ 20springboard_project.sql
@@ -32,14 +32,17 @@ commit;
 -- jpa
 -- jpaboard
 
-create table jpaboard(
-boardNo number,
-title varchar2(200) not null,
-content VARCHAR(1000),
-writer VARCHAR2(100) not null,
-regDate Date default sysdate not null,
-CONSTRAINT pk_jpaboard_no PRIMARY key(boardNo)
+-- jpa board
+CREATE TABLE jpaboard( 
+    board_no NUMBER, 
+    title VARCHAR2(200) NOT NULL, 
+    content VARCHAR2(1000), 
+    writer VARCHAR2(100) NOT NULL, 
+    reg_date DATE DEFAULT SYSDATE NOT NULL,
+    CONSTRAINT pk_jpaboard_no PRIMARY KEY (board_no) 
 );
+
+drop table jpaboard
 
 create sequence jpaboard_seq 
 start with 1 
